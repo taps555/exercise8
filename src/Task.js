ee8ae31aea1cbf768dc8179042ead00b0d52e49e
trioadhi <trioadhipamungkas04@gmail.com>
2024-10-28 18:30:35 +0700

    hello

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "TRIO ADHI PAMUNGKAS S"; // TODO: replace this
+const studentId = "FS11620401"; // TODO: replace this
+
+export { url, studentName, studentId };
